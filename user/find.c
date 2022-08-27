d5e8bcaf7bc234caf5187cb09e3c1b6496e230b0
pyq <eilo.pengyq@gmail.com>
2022-08-27 02:20:14 -0700

    finish lab1-4(find)

---
+++ user/find.c
@@ -0,0 +1,83 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+
+char *fmtname(char *path){
+  static char buf[DIRSIZ+1];
+  char *p;
+
+  // Find first character after last slash.
+  for(p=path+strlen(path); p >= path && *p != '/'; p--)
+    ;
+  p++;
+
+  if(strlen(p) >= DIRSIZ)
+    return p;
+  memmove(buf, p, strlen(p));
+  memset(buf+strlen(p), 0, DIRSIZ-strlen(p)); 
+  return buf;
+}
+
+void find(char *path, char *filename){
+    char buf[512], *p; //dir of file
+    int fd;            //file descriptor
+    struct dirent de;  //structure of the dir
+    struct stat st;    //structure of the file information
+
+    if((fd = open(path, 0)) < 0){
+        fprintf(2, "find: cannot open %s\n", path);
+        return;
+    }
+
+    if(fstat(fd, &st) < 0){
+        fprintf(2, "find: cannot stat %s\n", path);
+        close(fd);
+        return;
+    }
+
+    if( strcmp(filename, fmtname(path)) == 0){
+        printf("%s\n", path);
+    }
+
+    switch(st.type){
+    case T_FILE:
+        break;
+
+    case T_DIR:
+        if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+            printf("find: path too long\n");
+            break;
+        }
+        strcpy(buf, path);
+        p = buf+strlen(buf);
+        *p++ = '/';
+        //scan each file of the dir
+        while(read(fd, &de, sizeof(de)) == sizeof(de)){
+            if(de.inum == 0)
+                continue;
+            if(strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
+                continue;
+            memmove(p, de.name, DIRSIZ); //splice path
+            p[DIRSIZ] = 0;               //add string terminator
+            if(stat(buf, &st) < 0){
+                printf("find: cannot stat %s\n", buf);
+                continue;
+            }
+            find(buf, filename);
+        }
+        break;
+    }
+    close(fd);
+}
+
+int main(int argc, char *argv[]){
+    if(argc != 3){
+        fprintf(2, "Usage: find <directory> <file>\n");
+        exit(1);
+    }
+    else{
+        find(argv[1], argv[2]);
+        exit(0);            
+    }
+}
--- user/sleep.c
+++ user/sleep.c
@@ -1,10 +1,9 @@
 #include "kernel/types.h"
-#include "kernel/stat.h"
 #include "user/user.h"
 
 int main(int argc, char *argv[]){
     if(argc < 2){
-        fprintf(2, "Usage: sleep ${time}\n");
+        fprintf(2, "Usage: sleep <time>\n");
         exit(1);
     }
     sleep(atoi(argv[1]));
